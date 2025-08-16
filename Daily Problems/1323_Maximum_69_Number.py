5c98e47b3ecd56636d24ab36252226f272a24f80
Palash Shah <167026964+uncodingthecode@users.noreply.github.com>
2025-08-16 19:06:36 +0530

    Create 1323_Maximum_69_Number.py

---
+++ Daily Problems/1323_Maximum_69_Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num_str = list(str(num))
+        for i in range(len(num_str)):
+            if num_str[i] == '6':
+                num_str[i] = '9'
+                break
+        return int(''.join(num_str))
+# time complexity : O(n) || space complexity : O(1)
